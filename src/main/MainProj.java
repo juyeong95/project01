1036f566cfa86a1b801518c17e18b34ddd2f6f33
juyoung <bn0406@naver.com>
2021-08-26 17:56:13 +0900

    case 1 완료

--- src/main/MainProj.java
+++ src/main/MainProj.java
@@ -1,16 +1,20 @@
 package main;
 
 import java.util.Scanner;
+import juyeong.Jy;
 
 public class MainProj {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		Jy n = new Jy();
 		int num;
 		while(true) {
 			System.out.println("1.이주영님 2.성기룡님 3.설지희님 4.서광훈님 5.송영관님");
 			num=input.nextInt();
 			switch(num) {
-			case 1: break;
+			case 1:
+				System.out.println(n.jy);
+				break;
 			case 2: break;
 			case 3: break;
 			case 4: break;
